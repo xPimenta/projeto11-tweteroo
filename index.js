3c80b122dfbeed462e45a046c2b8b5a2af2121fb
Pimenta <mateuspimartins@hotmail.com>
2022-04-25 00:48:59 -0300

    login and post

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-import express from "express"
-import cors from "cors"
+import express from 'express'
+import cors from 'cors'
 
-const app = express()
-app.use(cors())
+let user = []
+let tweets = []
+
+const app = express();
+app.use(cors());
 app.use(express.json())
 
-const users = []
-const tweets = []
+app.post("/sign-up", (req,res) =>{
+    const signInData = req.body;
+    user.push(signInData);
+    res.send("OK")
+})
 
+app.post("/tweets", (req,res)=>{
+    const tweetData = req.body;
+    const userData = user.find(user => user.username === tweetData.username)
+    tweetData.avatar =  userData.avatar
+    tweets.push(tweetData);
+    if (!userData){
+        res.send("User not found")
+        return 
+    }
+    res.send("OK")
+})
 
-app.listen(5000, () => {
-	console.log("Server is running on port 5000")
+app.get("/tweets", (req,res)=>{
+    res.send(tweets.slice(-10));
+})
+
+app.listen(5000, ()=>{
+    console.log("Server is running on: http://localhost:5000")
 })
